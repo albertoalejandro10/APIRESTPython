4cc735bbcfcf6303a11d16c0c8194afe5e839399
Alberto <alberto196g@gmail.com>
2020-11-26 23:57:43 -0400

    Ya la API REST realiza todas las operaciones de CRUD y ademas puedes buscar productos por nombre, falta establecer la logica para el rango de precios

--- app.py
+++ app.py
@@ -4,13 +4,14 @@ app = Flask(__name__)
 
 from products import products
 
-@app.route('/ping')
-def ping():
-    return jsonify({"message": "Pong!"})
+# @app.route('/ping')
+# def ping():
+#     return jsonify({"message": "Pong!"})
 
 @app.route('/products')
 def getProducts():
     return jsonify({"products": products, "message": "Product's List"})
+
 
 @app.route('/products/<string:product_name>')
 def getProduct(product_name):
@@ -19,28 +20,45 @@ def getProduct(product_name):
         return jsonify({"product": productsFound[0]})
     return jsonify({"message": "Product not found"})
 
+
+
+@app.route('/products/prices/<int:product_price_a>&<int:product_price_b>')
+def getProductByPrice(product_price_a, product_price_b):
+    for product in products:
+        if product_price_a 
+    productsFounds = 
+    if(len(productsFound) > 0):
+        return jsonify({"product": productsFounds})
+    return jsonify({"message": "Products by prices not found"})
+
+
 @app.route('/products', methods=['POST'])
 def addProduct():
     new_product = {
         "name": request.json['name'],
+        "description": request.json['description'],
         "price": request.json['price'],
-        "quantity": request.json['quantity'],
     }
     products.append(new_product)
     return jsonify(({"message": "Product Added Succesfully", "products": products}))
+
+
 
 @app.route('/products/<string:product_name>', methods=['PUT'])
 def editProduct(product_name):
     productFound = [product for product in products if product['name'] == product_name]
     if(len(productFound) > 0):
         productFound[0]['name'] = request.json['name']
+        productFound[0]['description'] = request.json['description']
         productFound[0]['price'] = request.json['price']
-        productFound[0]['quantity'] = request.json['quantity']
         return jsonify({
             "message": "Product Updated",
             "product": productFound[0]
         })
     return jsonify({"message": "Product Not Found"})
+
+
+
 
 @app.route('/products/<string:product_name>', methods=['DELETE'])
 def deleteProduct(product_name):
@@ -48,9 +66,12 @@ def deleteProduct(product_name):
     if len(productsFound) > 0:
         products.remove(productsFound[0])
         return jsonify({
-            "message": "Product deleted",
+            "message": "Product Deleted",
             "products": products
         })
     return jsonify({"message": "Product Not Found"})
+
+
+
 if __name__ == '__main__':
     app.run(debug=True, port=4000)
--- products.py
+++ products.py
@@ -1,5 +1,128 @@
 products = [
-    {"name": "laptop", "price": 800, "quantity": 4},
-    {"name": "mouse", "price": 40, "quantity": 10},
-    {"name": "monitor", "price": 400, "quantity": 3},
+    {
+        "id":1,
+        "name":"Serie_Instinct",
+        "description":"Resistentes y fiables relojes con GPS para actividades al aire libre con funciones tácticas opcionales",
+        "price": 199990
+    },
+    {
+        "id":2,
+        "name":"Descent_Mk2_Series",
+        "description":"Computadoras de buceo estilo reloj con integración de aire opcional, navegación GPS de superficie, Pulse Ox, funciones multideporte y más.",
+        "price": 1299990
+    },
+    {
+        "id":3,
+        "name":"Vivofit_jr3",
+        "description":"Quieres un monitor de ejercicio para tu hijo que vaya un paso más alla. Con cuestionarios, juegos y aventuras educativas en una aplicación controlada por los padres, tu hijo podrá desbloquear más funciones a medida que realiza actividades.",
+        "price":69990
+    },
+    {
+        "id":4,
+        "name":"Serie_Venu_Sq",
+        "description":"Reloj inteligente con GPS, una pantalla impresionante y almacenamiento para tu música favorita en determinadas versiones. Y además, mantiene tu nivel de actividad y sintonía con tu cuerpo",
+        "price":199990
+    },
+    {
+        "id":5,
+        "name":"Serie_Forerunner_745",
+        "description":"Reloj inteligente para running y triatlón con GPS avanzado.",
+        "price":469990
+    },
+    {
+        "id":6,
+        "name":"Serie_fénix",
+        "description":"Relojes multideporte premium con GPS",
+        "price":699990
+    },
+    {
+        "id":7,
+        "name":"Venu",
+        "description":"Reloj inteligente GPS con pantalla AMOLED, aplicaciones de deporte integradas, capacidad para realizar pagos contactless, música y control de la salud durante todo el día",
+        "price":299990
+    },
+    {
+        "id":8,
+        "name":"Vívoactive_44S",
+        "description":"Relojes inteligentes con GPS en dos tamaños, con capacidad para realizar pagos sin contacto, aplicaciones de deporte integradas, música y control de la salud durante todo el día",
+        "price":249990
+    },
+    {
+        "id":9,
+        "name":"Vivomove_series",
+        "description":"Elegantes relojes inteligentes híbridos con pantallas táctiles ocultas",
+        "price":199990
+    },
+    {
+        "id":10,
+        "name":"Forerunner_945",
+        "description":"Reloj inteligente premium para carrera triatlón con GPS y música",
+        "price":499990
+    },
+    {
+        "id":11,
+        "name":"Forerunner_245245_Music",
+        "description":"Reloj inteligente para carrera con GPS, funciones de entrenamiento avanzadas y almacenamiento opcional de música",
+        "price":249990
+    },
+    {
+        "id":12,
+        "name":"Forerunner_4545S",
+        "description":"Relojes inteligente con GPS que admiten planes de entrenamiento de Garmin Coach",
+        "price":149990
+    },
+    {
+        "id":13,
+        "name":"Approach_S62",
+        "description":"Reloj de golf prémium con GPS, caddie virtual integrado, funciones avanzadas y un sistema de seguimiento de palos automático",
+        "price":449990
+    },
+    {
+        "id":14,
+        "name":"VivosmartO4",
+        "description":"Liviano tracker de actividad inteligente, con sensores de ritmo cardiaco y Pulse Ox en la muñeca, y monitores del sueño y la energía",
+        "price":99990
+    },
+    {
+        "id":15,
+        "name":"Garmin_Swim 2",
+        "description":"Reloj inteligente de natación con GPS para piscina y aguas abiertas",
+        "price":199990
+    },
+    {
+        "id":16,
+        "name":"Vívofit_4",
+        "description":"Tracker de actividad con pantalla a color siempre encendida y batería por un año sin recargas",
+        "price":79990
+    },
+    {
+        "id":17,
+        "name":"Approach_S40",
+        "description":"Elegante reloj inteligente de golf con GPS y pantalla táctil a color",
+        "price":249990
+    },
+    {
+        "id":18,
+        "name":"D2_Delta_Series",
+        "description":"Premium GPS aviator watches in 3 sizes; with color mapping, NEXRAD weather, logbook, music, Garmin Pay",
+        "price":899990
+    },
+    {
+        "id":19,
+        "name":"MARQ_Collection",
+        "description":" contactless payments, heart rate and optional Pulse Ox",
+        "price":1399990
+    },
+    {
+        "id":20,
+        "name":"Forerunner_735XT",
+        "description":"Reloj multideporte GPS con frecuencia cardiaca a través de la muñeca",
+        "price":"AGOTADO"
+    },
+    {
+        "id":21,
+        "name":"D2_Air",
+        "description":"Reloj Inteligente con GPS para aviación con una intensa pantalla táctil.",
+        "price":499990
+    }
 ]
